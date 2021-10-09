f102e51f5f7d7a297cab98d28abb8c6eb0312169
Rupinder Gill <rupinder0007gill@gmail.com>
2021-10-09 09:26:41 +0530

    Only super admin can update the role of any user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do |_exception|
+    redirect_to root_path, alert: 'Access denied. You are not authorized to access the requested page.'
+  end
 end
