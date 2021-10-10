cfde9e14e94468051aaaf604fb9606bdd85d90aa
Rupinder Gill <rupinder0007gill@gmail.com>
2021-10-10 23:01:44 +0530

    fix precomplie issue on heroku.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'bootstrap'
+import 'bootstrap';
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("channels");
 
 require('jquery');
 
-import "@fortawesome/fontawesome-free/css/all"
+import "@fortawesome/fontawesome-free/css/all";
 // Sub-directories
-import "../stylesheets/application"
+// import "../stylesheets/application";
